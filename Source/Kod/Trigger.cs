01100dfac27eeb8c3629c50e2e2646288009548c
HardworkerFree <hardworkerfree@gmail.com>
2022-05-10 15:36:41 +0300

    Text split; Trigger

---
+++ Source/Kod/Trigger.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shtoockie.Kod
+{
+    public class Trigger<TObservable> : ITrigger
+    {
+        private TObservable _previousObservableValue;
+        public TObservable PreviousObservableValue => _previousObservableValue;
+
+        private TObservable _currentObservableValue;
+        public TObservable CurrentObservableValue => _currentObservableValue;
+
+        private Func<TObservable> _observableValueGetter;
+        private Action _observableChangeHandler;
+
+        private Trigger(Func<TObservable> observableValueGetter, Action observableChangeHandler)
+        {
+            _observableValueGetter = observableValueGetter ?? DefaultGet;
+            _observableChangeHandler = observableChangeHandler ?? DefaultHandle;
+
+            _previousObservableValue = _observableValueGetter();
+            _currentObservableValue = _previousObservableValue;
+        }
+
+        public Trigger(object source, string sourcePropertyName)
+            : this(CreateObservableValueGetter(source, sourcePropertyName), null)
+        {
+        }
+
+        public Trigger(Action observableChangeHandler)
+            : this(null, observableChangeHandler)
+        {
+        }
+
+        public Trigger(object source, string sourcePropertyName, Action observableChangeHandler)
+            : this(CreateObservableValueGetter(source, sourcePropertyName), observableChangeHandler)
+        {
+        }
+
+        public void SetupSourceValueGetter(object source, string sourcePropertyName)
+        {
+            _observableValueGetter = CreateObservableValueGetter(source, sourcePropertyName);
+        }
+
+        public void SetupChangeHandler(Action observableChangeHandler)
+        {
+            _observableChangeHandler = observableChangeHandler ?? DefaultHandle;
+        }
+
+        private static Func<TObservable> CreateObservableValueGetter(object source, string observablePropertyName)
+        {
+            Type sourceType = source.GetType();
+            PropertyInfo sourcePropertyInfo = sourceType.GetProperty(observablePropertyName);
+
+            if (sourcePropertyInfo == null)
+            {
+                return DefaultGet;
+            }
+
+            MethodInfo sourcePropertyGetter = sourcePropertyInfo.GetGetMethod();
+
+            if (sourcePropertyGetter == null)
+            {
+                return DefaultGet;
+            }
+
+            return (Func<TObservable>)Delegate.CreateDelegate(typeof(Func<TObservable>), source, sourcePropertyGetter.Name);
+        }
+
+        private static TObservable DefaultGet()
+        {
+            return default(TObservable);
+        }
+
+        private void DefaultHandle()
+        {
+        }
+
+        public void Check()
+        {
+            _currentObservableValue = _observableValueGetter.Invoke();
+
+            if (!EqualityComparer<TObservable>.Default.Equals(_previousObservableValue, _currentObservableValue))
+            {
+                Fire();
+
+                _previousObservableValue = _currentObservableValue;
+            }
+        }
+
+        public void Fire()
+        {
+            _observableChangeHandler?.Invoke();
+        }
+    }
+}
--- Source/Literatura/Text.cs
+++ Source/Literatura/Text.cs
@@ -1,10 +1,13 @@
 ﻿namespace Shtoockie.Literatura
 {
     public readonly struct Text
-    {   private readonly bool _hasValue;
+    {
+        private readonly bool _hasValue;
         private readonly string _value;
 
         public static Text Empty = new Text(); 
+
+        public bool IsEmpty => string.IsNullOrEmpty(_value);
 
         #region Constructors
 
@@ -13,7 +16,7 @@
             _hasValue = !string.IsNullOrEmpty(text);
             _value = _hasValue ? text : string.Empty;
         }
-
+         
         #endregion // Constructors
 
         #region Basic operators
@@ -52,5 +55,28 @@
         }
 
         #endregion // Basic operators
+
+        #region Methods
+
+        public Text[] Split(char splitter)
+        {
+            if (!_hasValue)
+            {
+                return new Text[0];
+            }
+
+            string[] values = _value.Split(splitter);
+
+            Text[] result = new Text[values.Length];
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                result[i] = new Text(values[i]);
+            }
+
+            return result;
+        }
+
+        #endregion // Methods
     }
 }
