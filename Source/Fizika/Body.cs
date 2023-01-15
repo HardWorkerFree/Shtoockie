b6cc7f55c355d250f15c5bb8d3d51bce40b5f25c
HardworkerFree <hardworkerfree@gmail.com>
2023-01-16 02:15:40 +0300

    Body added

---
+++ Source/Fizika/Body.cs
@@ -0,0 +1,37 @@
+﻿using Shtoockie.Matematika;
+
+namespace Shtoockie.Fizika
+{
+	public abstract class Body
+    {
+		private Vector2N _position;
+		public Vector2N Position
+		{
+			get { return _position; }
+			protected set { _position = value; }
+		}
+
+		private Vector2N _movement;
+		public Vector2N Movement
+		{
+			get { return _movement; }
+			protected set { _movement = value; }
+		}
+
+		private Numerus _mass;
+		public Numerus Mass
+		{
+			get { return _mass; }
+			protected set { _mass = value; }
+		}
+
+		public Body(Numerus mass, Vector2N position, Vector2N movement)
+		{
+			_mass = mass;
+			_position = position;
+			_movement = movement;
+		}
+
+		public abstract bool CheckIntersection(RoundBody other);
+	}
+}
---
+++ Source/Fizika/RoundBody.cs
@@ -0,0 +1,28 @@
+﻿using Shtoockie.Matematika;
+
+namespace Shtoockie.Fizika
+{
+    public class RoundBody : Body
+    {
+		private Numerus _radius;
+
+		public Numerus Radius
+		{
+			get { return _radius; }
+			protected set { _radius = value; }
+		}
+
+		public RoundBody(Numerus mass, Vector2N position, Vector2N movement, Numerus radius)
+			: base(mass, position, movement)
+		{
+			_radius = radius;
+		}
+
+        public override bool CheckIntersection(RoundBody other)
+        {
+            Numerus distance = (this.Position + other.Position).Length();
+
+			return (distance - this._radius - other._radius) < Numerus.Zero;
+        }
+    }
+}
