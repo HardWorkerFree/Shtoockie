b9cb7bda9c79cdc515f049c1b76277754d2164a0
HardworkerFree <hardworkerfree@gmail.com>
2023-06-01 01:55:02 +0300

    Body extended

--- Source/Fizika/Body.cs
+++ Source/Fizika/Body.cs
@@ -12,11 +12,15 @@ namespace Shtoockie.Fizika
 		}
 
 		private Vector2N _movement;
-		public Vector2N Movement
-		{
-			get { return _movement; }
-			protected set { _movement = value; }
-		}
+		public Vector2N Movement => _movement;
+
+		private Vector2N _direction;
+		public Vector2N Direction => _direction;
+
+		private Numerus _speed;
+		public Numerus Speed => _speed;
+
+		public bool IsFixed => _movement == Vector2N.Zero;
 
 		private Numerus _mass;
 		public Numerus Mass
@@ -42,11 +46,29 @@ namespace Shtoockie.Fizika
 		public virtual void Redirect(Vector2N movement)
 		{
 			_movement = movement;
+			_speed = movement.Length();
+			_direction = movement / _speed;
+		}
+
+		public virtual void Slowdown(Numerus deltaTime, Numerus counterforce)
+		{
+			_speed -= counterforce * deltaTime;
+
+			if (_speed <= Numerus.Zero)
+			{
+				_speed = Numerus.Zero;
+				_direction = Vector2N.Zero;
+				_movement = Vector2N.Zero;
+
+				return;
+			}
+
+			_movement = _direction * _speed;
 		}
 
 		public virtual void Move(Numerus deltaTime)
 		{
 			_position = _position + _movement * deltaTime;
 		}
-	}
+    }
 }
