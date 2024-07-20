1939f85e140b5cc6dbab37e7415b406185fba4a2
HardworkerFree <hardworkerfree@gmail.com>
2024-07-21 02:44:34 +0300

    Binding list and ref added

---
+++ Source/Kod/BindingList.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Shtoockie.Kod
+{
+    public class BindingList<TItem>
+    {
+        private readonly List<TItem> _items;
+
+        private readonly Binding<int> _count;
+        public Binding<int> Count => _count;
+
+        private readonly HashSet<Action<TItem>> _addHandlers;
+        private readonly HashSet<Action<TItem>> _removeHandlers;
+
+        public BindingList()
+        {
+            _items = new List<TItem>();
+            _count = new Binding<int>(0);
+            _addHandlers = new HashSet<Action<TItem>>();
+            _removeHandlers = new HashSet<Action<TItem>>();
+        }
+
+        public void SubscribeAddHandler(Action<TItem> addHandler)
+        {
+            _addHandlers.Add(addHandler);
+        }
+
+        public void SubscribeRemoveHandler(Action<TItem> removeHandler)
+        {
+            _removeHandlers.Add(removeHandler);
+        }
+
+        public void UnsubscribeAddHandler(Action<TItem> addHandler)
+        {
+            _addHandlers.Remove(addHandler);
+        }
+
+        public void UnsubscribeRemoveHandler(Action<TItem> removeHandler)
+        {
+            _removeHandlers.Remove(removeHandler);
+        }
+
+        public void Add(TItem item)
+        {
+            _items.Add(item);
+
+            foreach (var addHandler in _addHandlers)
+            {
+                addHandler(item);
+            }
+
+            _count.Value = _items.Count;
+        }
+
+        public void Remove(TItem item)
+        {
+            _items.Remove(item);
+
+            foreach (var removeHandler in _removeHandlers)
+            {
+                removeHandler(item);
+            }
+
+            _count.Value = _items.Count;
+        }
+
+        public void Clear()
+        {
+            _items.Clear();
+            _count.Value = _items.Count;
+        }
+    }
+}
---
+++ Source/Kod/BindingRef.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Shtoockie.Kod
+{
+    public class BindingRef<TValue> : IBinding<TValue>, IReadonlyBinding<TValue> where TValue : class
+    {
+		private TValue _value;
+		public TValue Value
+		{
+			get { return _value; }
+			set { Update(value); }
+		}
+
+		private readonly HashSet<Action<TValue, TValue>> _subscribers;
+
+		public BindingRef(TValue initialValue = null)
+		{
+			_value = initialValue;
+			_subscribers = new HashSet<Action<TValue, TValue>>();
+		}
+
+		public void Subscribe(Action<TValue, TValue> action)
+		{
+			_subscribers.Add(action);
+		}
+
+		public void Unsubscribe(Action<TValue, TValue> action)
+		{
+			_subscribers.Remove(action);
+		}
+
+		public void Update(TValue value)
+		{
+			if (_value != value)
+			{
+				TValue oldValue = _value;
+				_value = value;
+
+				foreach (var subscriber in _subscribers)
+				{
+					subscriber(oldValue, _value);
+				}
+			}
+		}
+
+		public void ForcedUpdate(TValue value)
+		{
+            TValue oldValue = _value;
+            _value = value;
+
+            foreach (var subscriber in _subscribers)
+            {
+                subscriber(oldValue, _value);
+            }
+        }
+
+		public void SilentUpdate(TValue value)
+		{
+            _value = value;
+        }
+	}
+}
